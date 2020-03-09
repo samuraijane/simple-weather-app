ad35da6f2d5816ae6b827dc1c299346910dd94eb
Matthew Day <matt.day@shakerinternational.com>
2020-03-09 17:36:06 -0400

    Add routes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
-const { APIKEY, PORT } = require("./config");
+const { PORT } = require("./config");
 const app = express();
 
 app.use("/", express.static(__dirname + "/public"));
+app.use("/assets", express.static(__dirname + "/src/assets"));
+app.use("/js", express.static(__dirname + "/src/js"));
 app.use("/src", express.static(__dirname + "/src"));
+app.use("/utils", express.static(__dirname + "/src/utils"));
 app.use("/views", express.static(__dirname + "/src/views"));
 
 app.get("/heartbeat", function(req, res) {
--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,25 @@ const views = {
   about: {
     id: "t-main",
     onInit: false,
+    scriptSrc: null,
     url: `${path}/main/about.html`
   },
   footer: {
     id: "t-footer",
     onInit: true,
+    scriptSrc: null,
     url: `${path}/footer`
   },
   header: {
     id: "t-header",
     onInit: true,
+    scriptSrc: null,
     url: `${path}/header`
   },
   home: {
     id: "t-main",
     onInit: true,
+    scriptSrc: "../js/modules/zipCodeSearch.js",
     url: `${path}/main/home.html`
   }
 };
@@ -25,16 +29,26 @@ const views = {
 window.onload = () => {
   Object.keys(views).forEach(view => {
     const theView = views[view];
-    if (theView.onInit) getView(theView.id, theView.url);
+    if (theView.onInit) getView(theView);
   });
 };
 
-const getView = (id, url) => {
-  const el = document.getElementById(id);
+const getView = view => {
+  const el = document.getElementById(view.id);
   const ajax = new XMLHttpRequest();
-  ajax.open("GET", url, false);
+  ajax.open("GET", view.url, false);
   ajax.send();
   el.innerHTML = ajax.responseText;
+  if (view.scriptSrc && view.scriptSrc.length > 0) loadScript(view.scriptSrc);
+};
+
+const loadScript = script => {
+  const theScript = document.createElement("script");
+  theScript.src = script;
+  theScript.type = "module";
+  theScript.async = true;
+  theScript.onload = () => console.log("theScript is loaded");
+  document.body.appendChild(theScript);
 };
 
 const load = page => {
